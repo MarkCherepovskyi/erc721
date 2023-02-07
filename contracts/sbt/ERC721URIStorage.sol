e0893e3d099a8e6f2b357d9de1d08406c68fccfa
mark.cherepovskyi <mark.cherepovskyi@gmail.com>
2023-02-07 11:53:17 +0200

    prepare body using openzeppelin's interfaces

--- contracts/sbt/ERC721URIStorage.sol
+++ contracts/sbt/ERC721URIStorage.sol
@@ -1,34 +1,62 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts (last updated v4.7.0) (token/ERC721/extensions/ERC721URIStorage.sol)
+
 pragma solidity ^0.8.0;
 
-contract ERC721URIStorage is ERC721 {
-    mapping(uint => string) _tokenURIs;
+import "./ERC721.sol";
 
-    function tokenURI(uint tokenID) public view virtual override _requireMinted(tokenID) {
-        string memory _tokenURI = _tokenURIs[tokenID];
-        string memory _base = _baseURI();
-        if (bytes(_base).length == 0) {
+/**
+ * @dev ERC721 token with storage based token URI management.
+ */
+abstract contract ERC721URIStorage is ERC721 {
+    using Strings for uint256;
+
+    // Optional mapping for token URIs
+    mapping(uint256 => string) private _tokenURIs;
+
+    /**
+     * @dev See {IERC721Metadata-tokenURI}.
+     */
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+        _requireMinted(tokenId);
+
+        string memory _tokenURI = _tokenURIs[tokenId];
+        string memory base = _baseURI();
+
+        // If there is no base URI, return the token URI.
+        if (bytes(base).length == 0) {
             return _tokenURI;
         }
+        // If both are set, concatenate the baseURI and tokenURI (via abi.encodePacked).
         if (bytes(_tokenURI).length > 0) {
-            return string(abi.encodePacked(_base, _tokenURI));
+            return string(abi.encodePacked(base, _tokenURI));
         }
 
-        return super.tokenURI(tokenID);
+        return super.tokenURI(tokenId);
     }
 
-    function _setTokenURI(
-        uint tokenID,
-        string memory _tokenURI
-    ) internal virtual _requireMinted(tokenID) {
-        _tokenURIs[tokenID] = _tokenURI;
+    /**
+     * @dev Sets `_tokenURI` as the tokenURI of `tokenId`.
+     *
+     * Requirements:
+     *
+     * - `tokenId` must exist.
+     */
+    function _setTokenURI(uint256 tokenId, string memory _tokenURI) internal virtual {
+        require(_exists(tokenId), "ERC721URIStorage: URI set of nonexistent token");
+        _tokenURIs[tokenId] = _tokenURI;
     }
 
-    function burn(uint tokenID) public o _requireMinted(tokenID) {
-        super.burn(tokenID);
+    /**
+     * @dev See {ERC721-_burn}. This override additionally checks to see if a
+     * token-specific URI was set for the token, and if so, it deletes the token URI from
+     * the storage mapping.
+     */
+    function _burn(uint256 tokenId) internal virtual override {
+        super._burn(tokenId);
 
-        if (bytes(_tokenURIs[tokenID]).length != 0) {
-            delete _tokenURIs[tokenID];
+        if (bytes(_tokenURIs[tokenId]).length != 0) {
+            delete _tokenURIs[tokenId];
         }
     }
 }
